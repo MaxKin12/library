fae58c9908b43b59474fa009148a54807e3613b4
MaxKin12 <maxkinchikov@gmail.com>
2024-09-28 17:54:14 +0300

    init commit

---
+++ additional-service/src/main/java/com/project/additionalservice/AdditionalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.additionalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionalServiceApplication.class, args);
+	}
+
+}
---
+++ additional-service/src/test/java/com/project/additionalservice/AdditionalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.additionalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditionalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
